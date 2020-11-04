a20d43968c6a688916b760e5d58aff24efef2db3
Edilson silva <edilson_js@yahoo.com.br>
2020-11-04 09:34:46 -0300

    Projeto de estudo NODE JS

--- index.js
+++ index.js
@@ -12,14 +12,43 @@ const app = express();
 //assim podemos manipular os dados
 const bodyParser = require("body-parser");
 
+//Para ler o arquivo de json que contém os
+//produtos que desejo exibir. Nós iremos
+//carregar o módulo do fs(file system).
+const fs = require("fs");
+
+//Vamos criar uma variável no formato de array
+//que irá guardar os produtos do arquivo loja
+var dadosprodutos = null;
+
+//Realizar a leitura do arquivo de texto.
+//Primeira parte é o nome do arquivo
+//Segunda parte é o enconding(tipo texto-com acento)
+//Terceira parte é a função de callback
+
+fs.readFile("./loja.json", "utf-8", function(err, texto) {
+  if (err) throw err;
+  dadosprodutos = JSON.parse(texto);
+});
+
 //Vamos iniciar os exemplos de utilização de
 //verbos HTTP
+
+var layout = [
+  {
+    header: "Loja de Produtos",
+    navegacao: "listar,cadastrar,atualizar,deletar",
+    main: "página do corpo",
+    footer: "Av. João Paulo, 45, Vila Nova - São Paulo - SP"
+  }
+];
 
 //GET
 //Quando o meu usuário deseja obter algum dados
 //do servidor.
-app.get("/dados", (req, res) => {
-  res.send("Você está no verbo GET");
+app.get("/listar", (req, res) => {
+  layout[0].main = dadosprodutos.produtos;
+  res.send(layout);
 });
 
 //POST
@@ -28,20 +57,62 @@ app.get("/dados", (req, res) => {
 //realizar autenticação
 //vamos usar o body-parser
 app.use(bodyParser.json());
-app.post("/dados", (req, res) => {
-  res.send(req.body);
+
+app.post("/cadastrar", (req, res) => {
+  dadosprodutos.produtos.push(req.body);
+
+  fs.writeFile("./loja.json", JSON.stringify(dadosprodutos), "utf-8", function(
+    err
+  ) {
+    if (err) throw err;
+    res.send("Dados cadastrados");
+  });
 });
 
 //PUT
 //Utilizado quando o usuário deseja realizar uma atualização
 //nos dados
-app.put("/dados", (req, res) => {
-  res.send("Você está no verbo PUT");
+app.put("/atualizar", (req, res) => {
+  var idenviado = req.body.idproduto;
+
+  //pegar a quantidade de produtos dentro do arquivo json
+  var qtd = dadosprodutos.produtos.length;
+
+  for (var i = 0; i < qtd; i++) {
+    if (idenviado == dadosprodutos.produtos[i].idproduto) {
+      dadosprodutos.produtos[i].nome = req.body.nome;
+      dadosprodutos.produtos[i].descricao = req.body.descricao;
+      dadosprodutos.produtos[i].preco = req.body.preco;
+      dadosprodutos.produtos[i].imagem = req.body.imagem;
+      break;
+    }
+  }
+
+  fs.writeFile("./loja.json", JSON.stringify(dadosprodutos), "utf-8", function(
+    err
+  ) {
+    if (err) throw err;
+    res.send("Dados atualizados com sucesso!");
+  });
 });
-//DELTE
+//DELETE
 //Utilizado quando o usuário deseja apagar algum dado
-app.delete("/dados", (req, res) => {
-  res.send("Você está no verbo DELETE");
+app.delete("/apagar", (req, res) => {
+  var idenviado = req.body.idproduto;
+  var qtd = dadosprodutos.produtos.length;
+  for (var i = 0; i < qtd; i++) {
+    if (idenviado == dadosprodutos.produtos[i].idproduto) {
+      dadosprodutos.produtos.splice(i, 1);
+
+      break;
+    }
+  }
+  fs.writeFile("./loja.json", JSON.stringify(dadosprodutos), "utf-8", function(
+    err
+  ) {
+    if (err) throw err;
+    res.send("Produto apagado.");
+  });
 });
 
 app.listen(3000);
